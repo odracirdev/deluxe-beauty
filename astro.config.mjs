e7c16d9cba8e917bd5f582a740a77731a04e687d
odracirdev <ricardocuauro93@gmail.com>
2024-08-17 01:36:01 -0400

    chore: :construction_worker: deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://odracirdev.github.io/deluxe-beauty',
+  // base: '/deluxe-beauty',
+});

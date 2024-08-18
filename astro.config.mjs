5481149bbfb7aef7e2a924ddf8f49f419595ab37
odracirdev <ricardocuauro93@gmail.com>
2024-08-17 21:05:50 -0400

    fix: :bug: change paths for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://odracirdev.github.io/deluxe-beauty',
-  // base: '/deluxe-beauty',
+  base: '/deluxe-beauty',
 });

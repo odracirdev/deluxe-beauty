4810d33c6466f2aba8da21fa0ff8fbab249c0967
odracirdev <ricardocuauro93@gmail.com>
2024-08-20 20:54:58 -0400

    chore: :green_heart: Fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odracirdev.github.io/deluxe-beauty',
   base: '/deluxe-beauty',
 });

ec6d7fd1a1bf4032975e0ac7c6e7650bbb189e4c
odracirdev <ricardocuauro93@gmail.com>
2024-08-28 01:02:37 -0400

    chore: :heavy_plus_sign: add typescript dependences

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginAstro from 'eslint-plugin-astro';
-
+import tseslint from 'typescript-eslint'
 
 export default [
   {files: ['**/*.{js,mjs,cjs,astro']},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {ignores: ['dist/*', 'node_modules/*', '.github/*']}
 ];

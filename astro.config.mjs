d915b066d988c72881c19ccd41538a6c25a97b87
odracirdev <ricardocuauro93@gmail.com>
2024-09-01 02:24:48 -0400

    chore: :heavy_plus_sign: add imagetools component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
+import sitemap from 'astro-sitemap';
+import { astroImageTools } from 'astro-imagetools'
+
+const website  = 'https://deluxebeauty.cl';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://deluxebeauty.cl',
-  integrations: [icon()]
+  site: website, 
+  integrations: [
+    icon(),
+    astroImageTools,
+    sitemap({
+      canonicalURL: website ,
+      lastmod: new Date(),
+      createLinkInHead: false,
+      xmlns: {
+        xhtml: true
+      }})]
 });

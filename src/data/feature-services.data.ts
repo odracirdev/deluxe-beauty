64e64926a7fb2af10dc8a3b953224c1907e4aaa0
odracirdev <ricardocuauro93@gmail.com>
2024-10-30 19:13:57 -0300

    feat: ✨ create a separate file from home for the featured services

---
+++ src/data/feature-services.data.ts
@@ -0,0 +1,17 @@
+export const featuredServices = [
+  {
+    image: "/images/manos-y-pies/portada-manos-y-pies.avif",
+    name: "Manos y Pies",
+    slug: "/servicios/manos-y-pies/",
+  },
+  {
+    image: "/images/cejas-y-pestanas/laminado-de-cejas.avif",
+    name: "Cejas y Pestañas",
+    slug: "/servicios/cejas-y-pestanas/",
+  },
+  {
+    image: "/images/peluqueria/brushing.avif",
+    name: "Peluquería",
+    slug: "/servicios/peluqueria/",
+  },
+];
